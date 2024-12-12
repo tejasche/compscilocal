cd53ce88012795aa5fc389ccdbb51e14f58a65e7
tejas <tcherukuru165@g.fhsdschools.org>
2024-12-12 08:10:45 -0600

    work on 1.2.3

--- unit 1/1.2/1.2.3/main.py
+++ unit 1/1.2/1.2.3/main.py
@@ -6,7 +6,7 @@ apple_image = "apple.gif" # Store the file name of your shape
 pear_image = "pear.gif"
 
 wn = trtl.Screen()
-wn.setup(width=1.0, height=1.0)
+wn.setup(width=607, height=406)
 wn.addshape(apple_image) # Make the screen aware of the new file
 wn.addshape(pear_image)
 wn.bgpic("background.gif")
@@ -17,6 +17,7 @@ text = trtl.Turtle()
 text.penup()
 text.hideturtle()
 apple.penup()
+apple.speed(1)
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
 def drawapple(active_apple):
@@ -24,16 +25,18 @@ def drawapple(active_apple):
   wn.update()
 
 def falling():
-  apple.goto(apple.xcor(), apple.ycor() - 1)
+  apple.goto(apple.xcor(), apple.ycor() - 300)
   
 def drawtext(input):
-  text.goto(apple.xcor(), apple.ycor())
-  text.write(input, font = ("Arial", 16, "normal"))
+  text.goto(apple.xcor() - 23, apple.ycor() - 45)
+  text.write(input, font = ("Arial", 55, "normal"))
 
 #-----function calls-----
 drawapple(apple)
-drawtext("a")
-wn.onkeypress(falling, "a")
-wn.listen()
+drawtext("A")
 
+if wn.onkeypress(falling, "a"):
+  text.clear()
+
+wn.listen()
 wn.mainloop()
