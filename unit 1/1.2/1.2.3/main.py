fc9bc9ab066e3353b7a51ca851cdd3abe6b7ea73
tejas <tcherukuru165@g.fhsdschools.org>
2024-12-17 08:12:21 -0600

    work on 1.2.3

--- unit 1/1.2/1.2.3/main.py
+++ unit 1/1.2/1.2.3/main.py
@@ -1,5 +1,6 @@
 #   a123_apple_1.py
 import turtle as trtl
+import random as rand
 
 #-----setup-----
 apple_image = "apple.gif" # Store the file name of your shape
@@ -11,6 +12,8 @@ wn.addshape(apple_image) # Make the screen aware of the new file
 wn.addshape(pear_image)
 wn.bgpic("background.gif")
 
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
 apple = trtl.Turtle()
 text = trtl.Turtle()
 
@@ -21,20 +24,32 @@ apple.speed(0.6)
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
 def drawapple(active_apple):
-  active_apple.shape(pear_image)
-  wn.update()
+	active_apple.shape(pear_image)
+	wn.update()
 
 def falling():
-  text.clear()
-  apple.goto(apple.xcor(), apple.ycor() - 300)
+	text.clear()
+	apple.goto(apple.xcor(), apple.ycor() - 300)
   
 def drawtext(input):
-  text.goto(apple.xcor() - 23, apple.ycor() - 45)
-  text.write(input, font = ("Arial", 55, "normal"))
+	text.goto(apple.xcor() - 23, apple.ycor() - 45)
+	text.write(input, font = ("Arial", 55, "normal"))
+  
+def newlocation(turt):
+	if len(letters) > 0:
+		turt.goto(rand.randint(-200, 200), rand.randint(-200, 200))
+	
+def newletter():
+	return letters.pop(rand.randint(0, len(letters) - 1))
+
+def newturtle(apple):
+	apple.shape(apple_image)
+	drawtext(newletter())
 
 #-----function calls-----
 drawapple(apple)
 drawtext("A")
+
 wn.onkeypress(falling, "a")
 
 wn.listen()
